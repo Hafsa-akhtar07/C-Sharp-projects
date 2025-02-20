1218a755e52c5f515b6f3c4969e724e78e5ecbeb
Hafsa Akhtar <hafsaakhtar2135@gmail.com>
2025-02-20 17:47:16 +0500

    Employee Management System

---
+++ DTO/EmployeeDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class EmployeeDTO
+    {
+        public string eName { get; set; }
+        public float eID { get; set; }
+        public string eDept { get; set; }
+        public string eJoiningDate { get; set; }
+        public float eSalary { get; set; }
+
+    }
+
+    
+}
